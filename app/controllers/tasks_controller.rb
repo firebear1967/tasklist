6cb1ecce3ae0f1f9881dae1d08342aff727acded
firebear <firebear.business@gmail.com>
2021-02-07 06:12:02 +0000

    Initial Commit

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,61 @@
+class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page]).per(5)
+  end
+
+  def show
+  end
+  
+  def new
+    @task = current_user.tasks.build
+  end
+  
+  def create
+    @task = current_user.tasks.build(task_params)
+    if @task.save
+      flash[:success] = 'Task が正常に投稿されました'
+      redirect_to root_url
+    else
+      flash.now[:danger] = 'Task が投稿されませんでした'
+      render :new
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @task.update(task_params)
+      flash[:success] = 'Task は正常に更新されました'
+      redirect_to root_url
+    else
+      flash.now[:danger] = 'Task は更新されませんでした'
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+    flash[:success] = 'Task は正常に削除されました'
+    redirect_to tasks_url
+  end
+
+end
+
+
+private
+
+def set_task
+  @task = current_user.tasks.find_by(id: params[:id])
+  unless @task
+    redirect_to root_url
+  end
+end
+
+# Strong Parameter
+def task_params
+  params.require(:task).permit(:content, :status)
+end
